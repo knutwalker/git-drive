41622391c699b8bc8d594abde823ad4a4b6c6a80
Paul Horn <dev@knutwalker.engineer>
2022-03-22 00:17:03 +0100

    Replace pico_args with clap

--- src/args.rs
+++ src/args.rs
@@ -1,326 +1,221 @@
 use crate::{
-    data::{Action, Command, Id, Kind, New, Provided},
+    data::{Action, Command, Id, New, Provided},
     Result,
 };
-use console::{style, Term};
-
-pub(crate) fn help(to: &Term) -> String {
-    let style = to.style();
-
-    let help = format!(
-                        r#"
-    {name} {version}
-{description}
-
-{usage}:
-    git-drive [COMMAND]
-
-{commands}:
-    {with}        Start driving with the specified navigator(s)
-    {alone}       Start driving alone
-    {show}        Show current navigators
-    {list}        List known navigators
-    {new}         Add a new navigator, either prompted for, or specified
-    {edit}        Edit navigator(s), either prompted for, or specified
-    {delete}      Deletes navigator(s), either prompted for, or specified
-    {me} <CMD>    Operate on the driver instead of the navigator
-      {me} {list}   List known drivers
-      {me} {new}    Add a new driver, either prompted for, or specified
-      {me} {edit}   Edit driver(s), either prompted for, or specified
-      {me} {delete} Deletes driver(s), either prompted for, or specified
-    {as}          Change driver seat
-    {help}        Prints this message or the help of the given subcommand(s)
-
-FLAGS:
-    {h_short}, {help_long}       Prints help information
-    {v_short}, {version_long}    Prints version information
-
-"#,
-        name = style.apply_to(env!("CARGO_PKG_NAME")).green().for_stderr(),
-        version = env!("CARGO_PKG_VERSION"),
-        description = env!("CARGO_PKG_DESCRIPTION"),
-        usage = style.apply_to("USAGE").yellow(),
-        commands = style.apply_to("COMMANDS").yellow(),
-        with = style.apply_to("with").green(),
-        alone = style.apply_to("alone").green(),
-        show = style.apply_to("show").green(),
-        list = style.apply_to("list").green(),
-        new = style.apply_to("new").green(),
-        edit = style.apply_to("edit").green(),
-        delete = style.apply_to("delete").green(),
-        me = style.apply_to("me").green(),
-        as = style.apply_to("as").green(),
-        help = style.apply_to("help").green(),
-        h_short = style.apply_to("-h").green(),
-        help_long = style.apply_to("--help").green(),
-        v_short = style.apply_to("-V").green(),
-        version_long = style.apply_to("--version").green(),
-    );
-
-    help
-}
+use clap::{AppSettings::DeriveDisplayOrder, Args, IntoApp, Parser, Subcommand};
 
 pub(crate) fn print_help_stderr() -> Result<()> {
-    print_help(Term::stderr())
-}
-
-pub(crate) fn print_help(to: Term) -> Result<()> {
-    to.write_str(help(&to).as_str())?;
+    let mut out = std::io::stderr();
+    let mut app = AppArgs::command();
+    app.write_long_help(&mut out)?;
     Ok(())
 }
 
 pub(crate) fn command() -> Command {
-    match parse_args() {
-        Ok(c) => c,
-        Err(e) => e.handle(),
-    }
+    parse_args()
 }
 
-fn parse_args() -> Result<Command, ArgsErr> {
-    let mut args = pico_args::Arguments::from_env();
-    if args.contains(["-h", "--help"]) {
-        return Err(ArgsErr::HelpRequested);
-    }
-    if args.contains(["-V", "--version"]) {
-        return Err(ArgsErr::VersionRequested);
-    }
-
-    let cmd = match args.subcommand()? {
-        Some(cmd) => cmd,
-        None => {
-            args.finish()?;
-            return Ok(Command::nav(Action::Drive(Provided(None))));
-        }
-    };
-
-    let command = match cmd.as_str() {
-        "w" | "with" => Command::nav(Action::Drive(must_ids(args.free()?)?)),
-        "d" | "delete" => Command::nav(Action::Delete(ids(args.free()?))),
-        "as" => Command::drv(Action::Change(ids(args.free()?))),
-        "h" | "help" => return Err(ArgsErr::HelpRequested),
-
-        other => {
-            let mut command = match other {
-                "a" | "alone" => Command::nav(Action::Drive(Provided(Some(Vec::new())))),
-                "s" | "show" => {
-                    let color = match args.opt_value_from_str(["-c", "--color"]) {
-                        Ok(Some(c)) => c,
-                        Ok(None) => String::from("none"),
-                        Err(pico_args::Error::OptionWithoutAValue(_)) => String::from("cyan"),
-                        Err(e) => return Err(e.into()),
-                    };
-                    Command::nav(Action::Show(color, args.contains("--fail-if-empty")))
-                }
-                "l" | "list" => match args.subcommand()?.as_deref() {
-                    None => Command::nav(Action::List),
-                    Some("me") => Command::drv(Action::List),
-                    Some(other) => return Err(ArgsErr::UnknownCommand(format!("list {}", other))),
-                },
-                "n" | "new" => Command::nav(Action::New(new_from(&mut args, false)?)),
-                "e" | "edit" => Command::nav(Action::Edit(new_from(&mut args, false)?)),
-
-                "me" => match args.subcommand()?.ok_or(ArgsErr::MissingCommand)?.as_str() {
-                    "l" | "list" => Command::drv(Action::List),
-                    "n" | "new" => Command::drv(Action::New(new_from(&mut args, true)?)),
-                    "e" | "edit" => Command::drv(Action::Edit(new_from(&mut args, true)?)),
-                    "d" | "delete" => Command::drv(Action::Delete(Provided(None))),
-                    other => return Err(ArgsErr::UnknownCommand(format!("me {}", other))),
-                },
-                _ => return Err(ArgsErr::UnknownCommand(cmd)),
-            };
-            if let Command {
-                kind: Kind::Driver,
-                action: Action::Delete(provided),
-            } = &mut command
-            {
-                *provided = ids(args.free()?);
-            } else {
-                args.finish()?;
+fn parse_args() -> Command {
+    let args = AppArgs::parse();
+    match args.cmd {
+        None => Command::nav(Action::Drive(Provided(None))),
+        Some(cmd) => match cmd {
+            NavigatorCommand::With { ids } => Command::nav(Action::Drive(ids.into())),
+            NavigatorCommand::Delete { ids } => Command::nav(Action::Delete(ids.into())),
+            NavigatorCommand::As { ids } => Command::drv(Action::Change(ids.into())),
+            NavigatorCommand::Alone => Command::nav(Action::Drive(Provided(Some(Vec::new())))),
+            NavigatorCommand::Show {
+                color,
+                fail_if_empty,
+            } => {
+                let color = color
+                    .map(|value| value.unwrap_or_else(|| "cyan".to_string()))
+                    .unwrap_or_else(|| "none".to_string());
+                Command::nav(Action::Show(color, fail_if_empty))
             }
-
-            command
-        }
-    };
-
-    Ok(command)
-}
-
-fn ids(ids: Vec<String>) -> Provided {
-    provided(ids, true).unwrap()
-}
-
-fn must_ids(ids: Vec<String>) -> Result<Provided, ArgsErr> {
-    provided(ids, false)
-}
-
-fn provided(ids: Vec<String>, allow_empty: bool) -> Result<Provided, ArgsErr> {
-    let ids = ids.into_iter().map(Id).collect::<Vec<_>>();
-    let ids = if ids.is_empty() {
-        if !allow_empty {
-            return Err(ArgsErr::EmptyWith);
-        }
-        None
-    } else {
-        Some(ids)
-    };
-    Ok(Provided(ids))
-}
-
-fn new_from(args: &mut pico_args::Arguments, accept_key: bool) -> Result<New, ArgsErr> {
-    let alias1 = args.opt_value_from_str("--as")?;
-    let name = args.opt_value_from_str("--name")?;
-    let email = args.opt_value_from_str("--email")?;
-
-    let key = if accept_key {
-        let key1 = args.opt_value_from_str("--key")?;
-        let key2 = args.opt_value_from_str("--signingkey")?;
-        match (key1, key2) {
-            (None, None) => None,
-            (Some(k), None) => Some(k),
-            (None, Some(k)) => Some(k),
-            (Some(_), Some(_)) => return Err(ArgsErr::DuplicateFlags("--key", "--signingkey")),
-        }
-    } else {
-        None
-    };
-
-    let alias2 = args.subcommand()?;
-
-    let id = match (alias1, alias2) {
-        (None, None) => None,
-        (Some(id), None) => Some(id),
-        (None, Some(id)) => Some(id),
-        (Some(_), Some(_)) => return Err(ArgsErr::DuplicateFlags("<as>", "--as")),
-    };
-
-    let new = New {
-        id,
-        name,
-        email,
-        key,
-    };
-    Ok(new)
-}
-
-#[derive(Debug)]
-pub enum ArgsErr {
-    HelpRequested,
-    VersionRequested,
-    MissingCommand,
-    UnknownCommand(String),
-    DuplicateFlags(&'static str, &'static str),
-    EmptyWith,
-    ParserErr(pico_args::Error),
-}
-
-impl ArgsErr {
-    pub fn handle(self) -> ! {
-        let term = if self.to_stderr() {
-            Term::stderr()
-        } else {
-            Term::stdout()
-        };
-        let display = format!("{}", self);
-        let _ = term.write_str(&display);
-        if self.suggest_help() {
-            let _ = term.write_line("");
-            let suggestion = format!(
-                "For more information try {help}",
-                help = style("--help").green()
-            );
-            let _ = term.write_line(&suggestion);
-        }
-
-        std::process::exit(self.exit_code())
-    }
-
-    pub fn exit_code(&self) -> i32 {
-        match self {
-            ArgsErr::HelpRequested => 0,
-            ArgsErr::VersionRequested => 0,
-            ArgsErr::MissingCommand => 1,
-            ArgsErr::UnknownCommand(_) => 2,
-            ArgsErr::DuplicateFlags(_, _) => 3,
-            ArgsErr::EmptyWith => 4,
-            ArgsErr::ParserErr(pico_args::Error::UnusedArgsLeft(_)) => 5,
-            ArgsErr::ParserErr(pico_args::Error::OptionWithoutAValue(_)) => 6,
-            ArgsErr::ParserErr(_) => 7,
-        }
-    }
-
-    pub fn to_stderr(&self) -> bool {
-        !matches!(self, ArgsErr::HelpRequested | ArgsErr::VersionRequested)
-    }
-
-    pub fn suggest_help(&self) -> bool {
-        !matches!(
-            self,
-            ArgsErr::HelpRequested | ArgsErr::VersionRequested | ArgsErr::MissingCommand
-        )
-    }
-}
-
-impl std::fmt::Display for ArgsErr {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            ArgsErr::HelpRequested => write!(f, "{}", help(&Term::stdout())),
-            ArgsErr::VersionRequested => writeln!(
-                f,
-                concat!(env!("CARGO_PKG_NAME"), " ", env!("CARGO_PKG_VERSION"))
-            ),
-            ArgsErr::MissingCommand => write!(f, "{}", help(&Term::stderr())),
-            ArgsErr::UnknownCommand(c) => writeln!(
-                f,
-                "{error} The command '{cmd}' was not recognized",
-                error = style("error:").red(),
-                cmd = style(c).yellow(),
-            ),
-            ArgsErr::DuplicateFlags(a, b) => writeln!(
-                f,
-                "{error} The argument '{a}' cannot be used with '{b}'",
-                error = style("error:").red(),
-                a = style(a).yellow(),
-                b = style(b).yellow(),
-            ),
-            ArgsErr::EmptyWith => writeln!(
-                f,
-                "{error} The command '{with}' required at least one argument",
-                error = style("error:").red(),
-                with = style("with").yellow(),
-            ),
-            ArgsErr::ParserErr(pico_args::Error::UnusedArgsLeft(args)) => {
-                write!(
-                    f,
-                    "{error} Unknown arguments in this context:",
-                    error = style("error:").red()
-                )?;
-                for arg in args {
-                    write!(f, " {}", style(arg).yellow())?;
-                }
-                writeln!(f)
+            NavigatorCommand::List { cmd } => {
+                cmd.map_or(Command::nav(Action::List), |_| Command::drv(Action::List))
             }
-            ArgsErr::ParserErr(pico_args::Error::OptionWithoutAValue(arg)) => writeln!(
-                f,
-                "{error} The argument {arg} requires a value not non was supplied",
-                error = style("error:").red(),
-                arg = style(arg).yellow(),
-            ),
-            ArgsErr::ParserErr(e) => writeln!(f, "{error} {}", e, error = style("error:").red()),
-        }
+            NavigatorCommand::New { id } => Command::nav(Action::New((id, None).into())),
+            NavigatorCommand::Edit { id } => Command::nav(Action::Edit((id, None).into())),
+            NavigatorCommand::Me { cmd } => match cmd {
+                DriverCommand::List => Command::drv(Action::List),
+                DriverCommand::New { id, key } => Command::drv(Action::New((id, Some(key)).into())),
+                DriverCommand::Edit { id, key } => {
+                    Command::drv(Action::Edit((id, Some(key)).into()))
+                }
+                DriverCommand::Delete { ids } => Command::drv(Action::Delete(ids.into())),
+            },
+        },
     }
 }
 
-impl From<pico_args::Error> for ArgsErr {
-    fn from(v: pico_args::Error) -> Self {
-        ArgsErr::ParserErr(v)
+#[derive(Parser, Clone, Debug)]
+#[clap(
+    name = env!("CARGO_PKG_NAME"),
+    version = env!("CARGO_PKG_VERSION"),
+    about = env!("CARGO_PKG_DESCRIPTION"),
+    propagate_version = true,
+    infer_long_args = true,
+    infer_subcommands = true,
+    subcommand_required = false,
+    global_setting = DeriveDisplayOrder
+)]
+struct AppArgs {
+    #[clap(subcommand)]
+    cmd: Option<NavigatorCommand>,
+}
+
+#[derive(Subcommand, Clone, Debug)]
+enum NavigatorCommand {
+    /// Start driving with the specified navigator(s)
+    With {
+        /// The navigators
+        #[clap(min_values = 1, multiple_values = true, required = true)]
+        ids: Vec<String>,
+    },
+
+    /// Deletes navigator(s), either prompted for, or specified
+    Delete {
+        /// The navigators
+        #[clap(min_values = 0)]
+        ids: Vec<String>,
+    },
+
+    /// Change driver seat
+    As {
+        /// The navigators
+        #[clap(min_values = 0)]
+        ids: Vec<String>,
+    },
+
+    /// Start driving alone
+    Alone,
+
+    /// Show current navigators
+    Show {
+        /// The color in which to print the current navigators
+        #[clap(short, long, visible_alias = "colour")]
+        color: Option<Option<String>>,
+
+        /// If set, fail the process if there are no current navigators
+        #[clap(long)]
+        fail_if_empty: bool,
+    },
+
+    /// List known navigators
+    List {
+        #[clap(subcommand)]
+        cmd: Option<ListCommand>,
+    },
+
+    /// Add a new navigator, either prompted for, or specified
+    New {
+        #[clap(flatten)]
+        id: IdentityArgs,
+    },
+
+    ///Edit navigator(s), either prompted for, or specified
+    Edit {
+        #[clap(flatten)]
+        id: IdentityArgs,
+    },
+
+    /// Operate on the driver instead of the navigator
+    Me {
+        #[clap(subcommand)]
+        cmd: DriverCommand,
+    },
+}
+
+#[derive(Args, Clone, Debug)]
+struct IdentityArgs {
+    /// The identifier to use for the author's entry
+    #[clap(long, conflicts_with = "alias")]
+    r#as: Option<String>,
+
+    /// The author's name
+    #[clap(long)]
+    name: Option<String>,
+
+    /// The author's email
+    #[clap(long)]
+    email: Option<String>,
+
+    /// The identifier to use for the author's entry
+    #[clap(conflicts_with = "as")]
+    alias: Option<String>,
+}
+
+#[derive(Args, Clone, Debug)]
+struct KeyArgs {
+    /// The signing key to use
+    #[clap(long, visible_alias = "signingkey")]
+    key: Option<String>,
+}
+
+#[derive(Subcommand, Clone, Debug)]
+enum DriverCommand {
+    /// List known drivers
+    List,
+
+    /// Add a new driver, either prompted for, or specified
+    New {
+        #[clap(flatten)]
+        id: IdentityArgs,
+
+        #[clap(flatten)]
+        key: KeyArgs,
+    },
+    /// Edit driver(s), either prompted for, or specified
+    Edit {
+        #[clap(flatten)]
+        id: IdentityArgs,
+
+        #[clap(flatten)]
+        key: KeyArgs,
+    },
+    /// Deletes driver(s), either prompted for, or specified
+    Delete {
+        #[clap(min_values = 0)]
+        ids: Vec<String>,
+    },
+}
+
+#[derive(Subcommand, Clone, Debug)]
+enum ListCommand {
+    /// It's me
+    Me,
+}
+
+impl From<Vec<String>> for Provided {
+    fn from(ids: Vec<String>) -> Self {
+        let ids = ids.into_iter().map(Id).collect::<Vec<_>>();
+        let ids = if ids.is_empty() { None } else { Some(ids) };
+        Provided(ids)
     }
 }
 
-impl std::error::Error for ArgsErr {
-    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-        match self {
-            ArgsErr::ParserErr(e) => Some(e),
-            _ => None,
+impl From<(IdentityArgs, Option<KeyArgs>)> for New {
+    fn from(
+        (
+            IdentityArgs {
+                r#as,
+                name,
+                email,
+                alias,
+            },
+            key,
+        ): (IdentityArgs, Option<KeyArgs>),
+    ) -> Self {
+        let id = r#as.or(alias);
+        let key = key.and_then(|k| k.key);
+
+        New {
+            id,
+            name,
+            email,
+            key,
         }
     }
 }
